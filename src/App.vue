04eba2d92b4e238952edbb85e980be4bb1b5ff22
Andrea Tamm <tamm.andrea2005@gmail.com>
2024-11-29 11:35:00 +0200

    Add simple item list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup>
-import { ref } from 'vue';
-let message = ref ('Hello Vue!');
+import { ref } from "vue";
+let newItem = ref("");
+let items = ref(["piim", "viin", "leib", "õlu", "banaan"]);
 
-
+function add() {
+  if (newItem.value.trim() !== "") {
+    items.value.push(newItem.value);
+  }
+  newItem.value = '';
+}
 </script>
 
 <template>
-    <div class="container mt-2">
-        <input class="input" type="text" v-model="message">
-        <div class="content">
-            <h1>{{ message.split('').reverse().join('') }}</h1>
-        </div>
+  <div class="container mt-2">
+    <div class="field has-addons">
+      <div class="control is-expanded">
+        <input class="input" type="text" v-model="newItem" @keydown.enter="add">
+      </div>
+      <div class="control">
+        <button class="button is-info" @click="add">Add</button>
+      </div>
     </div>
+    <div class="content">
+      <ul>
+        <li v-for="item in items">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
