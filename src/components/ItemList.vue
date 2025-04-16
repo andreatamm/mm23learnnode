9ec083460cb33463ed592c01fc4469dd8743f097
Andrea Tamm <tamm.andrea2005@gmail.com>
2025-04-16 15:58:32 +0300

    aaaaah

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -3,11 +3,11 @@ defineProps(['items', 'title']);
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
-  <ul>
-    <li v-for="item in items">
-      {{ item.text }}
-      <input type="checkbox" v-model="item.done" />
-    </li>
-  </ul>
+    <h1>{{ title }}</h1>
+    <ul>
+        <li v-for="item in items">
+            {{ item.text }}
+            <input type="checkbox" v-model="item.done">
+        </li>
+    </ul>
 </template>
