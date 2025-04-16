26f7b23404ee80977f4b3ce64d39bcce3933410e
Andrea Tamm <tamm.andrea2005@gmail.com>
2025-04-16 15:56:42 +0300

    aaah

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,8 @@
 <script setup>
-import { ref, computed } from "vue";
-import ItemList from "./ItemList.vue";
-let newItem = ref("");
-let items = ref([
-  {text:"piim", done: true}, 
-  {text:"viin", done: false}, 
-  {text:"leib", done: true}, 
-  {text:"õlu", done: false}, 
-  {text:"banaan", done: true},
-]);
-
-function add() {
-  if (newItem.value.trim() !== "") {
-    items.value.push({test: newItem.value, done: false});
-  }
-  newItem.value = '';
-}
-
-let doneItems = computed(() => items.value.filter(item => item.done));
-let toDoItems = computed(() => items.value.filter(item => !item.done));
-
+import Btn from './components/Btn.vue';
 </script>
-
 <template>
   <div class="container mt-2">
-    <div class="field has-addons">
-      <div class="control is-expanded">
-        <input class="input" type="text" v-model="newItem" @keydown.enter="add">
-      </div>
-      <div class="control">
-        <button class="button is-info" @click="add">Add</button>
-      </div>
-    </div>
-    <div class="content">
-     <ItemList :items="items" title="All Items"></ItemList>
-     <ItemList :items="doneItems" title="Done Items"></ItemList>
-     <ItemList :items="toDoItems" title="ToDo Items"></ItemList>
-    </div>
+    <Btn color="primary" light>Button is cool</Btn>
   </div>
 </template>
